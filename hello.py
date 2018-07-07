5799042e60b105359b697769b48ae8b6980c0f48
Wilian Tapia <bennyrock20@hotmail.com>
2018-07-07 07:31:04 -0500

    more changes

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -6,10 +7,19 @@ def hello():
     return "Hello World!"
 
 
-@app.route("/<string:name>")
+@app.route("/name/<string:name>")
 def hi(name):
     return f"<h1>Hello,{name}!</h1>"
 
 @app.route('/html')
 def render_html():
-    return render_template('template.html')
+    head_line= "Flask"
+    return render_template('template.html', headline= head_line)
+
+@app.route('/is-new-year')
+def new_year():
+    head_line= "Flask"
+    now= datetime.datetime.now()
+    new_year = now.month == 1 and now.day == 1
+    names = ["Alice","Jane","Diana"]
+    return render_template('new_year.html', new_year= new_year, names = names)
