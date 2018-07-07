ac9fbabb2b41f2f15bd6addc4a30b5b858082356
Wilian Tapia <bennyrock20@hotmail.com>
2018-07-07 06:57:56 -0500

    Added template

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route("/<string:name>")
+def hi(name):
+    return f"<h1>Hello,{name}!</h1>"
+
+@app.route('/html')
+def render_html():
+    return render_template('template.html')
